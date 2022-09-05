074c78d23b6d957cb5f75a36b5170dc22a3fd770
Derek Peacock <derek.peacock@yahoo.com>
2022-09-05 19:23:59 +0100

    Added product and list

--- ExampleBlazorApp/Models/Product.cs
+++ ExampleBlazorApp/Models/Product.cs
@@ -4,13 +4,14 @@ namespace ExampleBlazorApp.Models
 {
     public class Product
     {
+        [Key]
         public int Id { get; set; } = 0;
 
         [StringLength(60), Required]
         public string Name { get; set; } = String.Empty;
-        
+
         [StringLength(255)]
-        public string Description { get; set; } = String.Empty;
+        public string? Description { get; set; } = null;
 
         [Range(0, 100.00)]
         public decimal Price { get; set; } = 0;
