f921af56d95a7f0b181fb14e888f47501b716d94
Derek Peacock <derek.peacock@yahoo.com>
2022-09-05 14:57:57 +0100

    Added product

---
+++ ExampleBlazorApp/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExampleBlazorApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; } = 0;
+
+        [StringLength(30), Required]
+        public string Name { get; set; } = String.Empty;
+        
+        [StringLength(255)]
+        public string Description { get; set; } = String.Empty;
+
+        [Range(0, 100.00)]
+        public decimal Price { get; set; } = 0;
+    }
+}
